9a0c1ce28dbe9a62d9365c81581bd21427bec924
Keith A Cissell <keithacissell@gmail.com>
2018-10-02 13:27:48 -0500

    Fixed most bugs. Sorting/selection isn't garunteeing a new population of the correct size.

--- GAStockCuttingSoln_v2.py
+++ GAStockCuttingSoln_v2.py
@@ -30,8 +30,8 @@ random.seed(datetime.now())
 
 NUMBER_OF_PIECES = 6  # HARDCODED
 STOCK_WIDTH = 800 # HARDCODED  Width of stock
-STOCK_HEIGHT = 400 # HARDCODED  Height of stock =
-NUMBER_OF_GENERATIONS = 10 # HARDCODED Number of generations of evolution
+STOCK_HEIGHT = 600 # HARDCODED  Height of stock =
+NUMBER_OF_GENERATIONS = 500 # HARDCODED Number of generations of evolution
 POPULATION_SIZE = 10  # HARDCODED Number of individuals in population
 
 piece_colors = ["gold", "deepskyblue", "green3", "tan1", "orchid1",
@@ -115,14 +115,14 @@ def fitness2(individual):
                     # Shape2 Left & Right X point values
                     lengthS2 = [shape2['x1'], shape2['x2']]
 
-                    if (lengthS1[0] <= lengthS2[1]<= lengthS1[1]) or (lengthS1[0] <= lengthS2[0]<= lengthS1[1]):
+                    if (lengthS1[0] <= lengthS2[1] and lengthS2[1] <= lengthS1[1]) or (lengthS1[0] <= lengthS2[0] and lengthS2[0] <= lengthS1[1]):
 
                         # Shape1 Top & Bottom y point values
                         heightS1 = [shape1['y1'], shape1['y2']]
                         # Shape2 Top & Bottom Y point values
                         heightS2 = [shape2['y1'], shape2['y2']]
 
-                        if (heightS1[0] <= heightS2[1]<= heightS1[1]) or (heightS1[0] <= heightS2[0]<= heightS1[1]):
+                        if (heightS1[0] <= heightS2[1] and heightS2[1] <= heightS1[1]) or (heightS1[0] <= heightS2[0] and heightS2[0] <= heightS1[1]):
 
                             #If two shapes overlap, continue
                             overlap_area = 0
@@ -163,7 +163,7 @@ def fitness2(individual):
     for key in indiv_set:
         overlap = indiv_set[key]
         total_overlap += overlap
-    print (total_overlap)
+    # print (total_overlap)
     return total_overlap
 
 
@@ -174,8 +174,8 @@ TBD -- this places the piece within the stock (as opposed to putting
 at least the UL corner but not necessarily the LR corner in stock)
 '''
 # https://www.w3schools.com/python/python_dictionaries.asp
-def makeRectObj(w, h, x1, y1, c):
-    return { "width": w, "height": h, "color": c,
+def makeRectObj(i, w, h, x1, y1, c):
+    return { "id": i, "width": w, "height": h, "color": c,
         "x1": x1, "y1": y1,
         "x2": x1+w, "y2": y1+h}  # Return a dictionary object
 
@@ -185,25 +185,37 @@ def crossover(population):
     for i in range(POPULATION_SIZE):
         selection1 = random.randint(0, POPULATION_SIZE - 1)
         selection2 = random.randint(0, POPULATION_SIZE - 1)
-        cut = random.randint(0, NUMBER_OF_PIECES)
+        cut = random.randint(0, NUMBER_OF_PIECES - 1)
 
-        new_individual = {"Pieces": [0 for j in range(NUMBER_OF_PIECES)],
-        		"Fitness1": None,
-        		"Fitness2": None }
+        new_individual = {  "Pieces": [0 for j in range(NUMBER_OF_PIECES)],
+                    		"Fitness1": None,
+                    		"Fitness2": None }
+        transferedPiecesIds = []
 
         for i in range(cut):
-            new_individual["Pieces"][i] = population[selection1]["Pieces"][i]
+            new_individual["Pieces"][i] = copy_piece(population[selection1]["Pieces"][i])
+            transferedPiecesIds.append(population[selection1]["Pieces"][i]["id"])
 
-        for i in range(cut, NUMBER_OF_PIECES):
-            print(new_individual["Pieces"][i])
-            print(population[selection2])
-            print(population[selection2]["Pieces"][i])
-            new_individual["Pieces"][i] = population[selection2]["Pieces"][i]
+        for i in range(NUMBER_OF_PIECES):
+            if not population[selection2]["Pieces"][i]["id"] in transferedPiecesIds:
+                new_individual["Pieces"][i] = copy_piece(population[selection2]["Pieces"][i])
+                # print(new_individual["Pieces"][i])
+                # print(population[selection2])
+                # print(population[selection2]["Pieces"][i])
 
         offsprings.append(new_individual)
 
     return offsprings
 
+
+def copy_piece(original):
+    copy = makeRectObj( original["id"],
+                        original["width"],
+                        original["height"],
+                        original["x1"],
+                        original["y1"],
+                        original["color"])
+    return copy
 
 # SELECTION HELPER FUNCTION DEFINITION
 def non_dominated_sort(population):
@@ -299,20 +311,26 @@ TBD -- this places the piece within the stock (as opposed to putting
 at least the UL corner but not necessarily the LR corner in stock)
 '''
 population = [0 for i in range(POPULATION_SIZE)]
+# initialize piece sizes
+piece_sizes = []
+for i in range(POPULATION_SIZE):
+    w = random.randint(50, 400)
+    h = random.randint(50, 400)
+    piece_sizes.append([w,h])
 for indiv_count in range(POPULATION_SIZE):
     # individual = [0 for j in range(NUMBER_OF_PIECES)]
     individual = {  "Pieces": [0 for j in range(NUMBER_OF_PIECES)],
                     "Fitness1": None,
                     "Fitness2": None }
     for piece_count in range(NUMBER_OF_PIECES):
-        w = 200  #  TBD HARDCODED
-        h = 200  #  TBD HARDCODED
+        w = piece_sizes[piece_count][0]
+        h = piece_sizes[piece_count][1]
         c = piece_colors[piece_count]  # TBD -- need more colors when more pieces
         x1 = random.randint(0, STOCK_WIDTH - w)   # piece is within stock
         y1 = random.randint(0, STOCK_HEIGHT - h)    # piece is within stock
 
         # An individual is an array of dictionary objects
-        individual["Pieces"][piece_count] = makeRectObj(w, h, x1, y1, c)
+        individual["Pieces"][piece_count] = makeRectObj(piece_count, w, h, x1, y1, c)
         #print(individual[piece_count])
         #print("	Piece ", piece_count, " x1 is ", (individual[piece_count]).get("x1"))
 
@@ -347,11 +365,33 @@ def copy_individual(original):
             "Fitness1": None,
             "Fitness2": None }
     for i in range(len(original["Pieces"])):
-        copy["Pieces"][i] = makeRectObj(  original["Pieces"][i]["width"],
+        copy["Pieces"][i] = makeRectObj(
+                                original["Pieces"][i]["id"],
+                                original["Pieces"][i]["width"],
                                 original["Pieces"][i]["height"],
                                 original["Pieces"][i]["x1"],
                                 original["Pieces"][i]["y1"],
                                 original["Pieces"][i]["color"])
+    return copy
+
+'''
+FULL COPY INDIVIDUAL
+Create a deep copy of an individual dictionary
+'''
+def full_copy_individual(original):
+    copy = {"Pieces": [0 for j in range(NUMBER_OF_PIECES)],
+            "Fitness1": None,
+            "Fitness2": None }
+    for i in range(len(original["Pieces"])):
+        copy["Pieces"][i] = makeRectObj(
+                                original["Pieces"][i]["id"],
+                                original["Pieces"][i]["width"],
+                                original["Pieces"][i]["height"],
+                                original["Pieces"][i]["x1"],
+                                original["Pieces"][i]["y1"],
+                                original["Pieces"][i]["color"])
+        copy["Fitness1"] = original["Fitness1"]
+        copy["Fitness2"] = original["Fitness2"]
     return copy
 
 '''
@@ -370,10 +410,10 @@ IN BOUNDS ROTATE
 Checks if rotation in bounds
 '''
 def in_bounds_rotate(piece):
-    return (piece["x1"] >= 0 and
-            piece["x2"] + piece["height"] <= STOCK_WIDTH and
-            piece["y1"] >= 0 and
-            piece["y2"] + piece["width"] <= STOCK_HEIGHT)
+    return (piece["x1"] + piece["height"] >= 0 and
+            piece["x1"] + piece["height"] <= STOCK_WIDTH and
+            piece["y1"] + piece["width"] >= 0 and
+            piece["y1"] + piece["width"] <= STOCK_HEIGHT)
 
 
 '''
@@ -389,7 +429,8 @@ A mutation MAY change:
     - Rotating (swapping the height and width of a piece)
 '''
 def create_mutated_copies(original_population, generation_num):
-    mutation_rate = (NUMBER_OF_GENERATIONS - generation_num) / NUMBER_OF_GENERATIONS
+    mutation_scale = generation_num / 10 if (generation_num / 10 > 1) else 1.0
+    mutation_rate = 1.0 * 0.9**(generation_num/mutation_scale)
     mutated_copies = []
     for i in range(len(original_population)):
         # Copy a random individual
@@ -400,7 +441,6 @@ def create_mutated_copies(original_population, generation_num):
         max_swaps = round(len(original_population) * mutation_rate)
         num_swaps = random.randint(0, max_swaps)
         for j in range(num_swaps):
-            mutated = True
             # Grab random indexes to swap positions
             indx1 = random.randint(0, len(copy["Pieces"]) - 1)
             indx2 = random.randint(0, len(copy["Pieces"]) - 1)
@@ -416,42 +456,38 @@ def create_mutated_copies(original_population, generation_num):
                 copy["Pieces"][indx1]["y1"] = y2
                 copy["Pieces"][indx1]["y2"] = y2 + copy["Pieces"][indx1]["height"]
             # Adjust piece 2
-            if in_bounds_move(copy["Pieces"][indx1], x1, y1):
+            if in_bounds_move(copy["Pieces"][indx2], x1, y1):
                 copy["Pieces"][indx2]["x1"] = x1
                 copy["Pieces"][indx2]["x2"] = x1 + copy["Pieces"][indx2]["width"]
                 copy["Pieces"][indx2]["y1"] = y1
                 copy["Pieces"][indx2]["y2"] = y1 + copy["Pieces"][indx2]["height"]
         # Moving Mutation
-        chance_of_move = .6 * mutation_rate
         max_move_dist_x = STOCK_WIDTH * mutation_rate / 2
         max_move_dist_y = STOCK_HEIGHT * mutation_rate / 2
         for indx in range(len(copy["Pieces"])):
-            randNum = random.uniform(0.0, 1.0)
-            if randNum < chance_of_move:
-                mutated = True
-                # Grab random individual
-                x_move = random.uniform(-max_move_dist_x, max_move_dist_x)
-                y_move = random.uniform(-max_move_dist_y, max_move_dist_y)
-                new_x = copy["Pieces"][indx]["x1"] + x_move
-                new_y = copy["Pieces"][indx]["y1"] + y_move
-                if in_bounds_move(copy["Pieces"][indx], new_x, new_y):
-                    copy["Pieces"][indx]["x1"] += x_move
-                    copy["Pieces"][indx]["x2"] += x_move
-                    copy["Pieces"][indx]["y1"] += y_move
-                    copy["Pieces"][indx]["y2"] += y_move
+            # Grab random individual
+            x_move = random.uniform(-max_move_dist_x, max_move_dist_x)
+            y_move = random.uniform(-max_move_dist_y, max_move_dist_y)
+            new_x = copy["Pieces"][indx]["x1"] + x_move
+            new_y = copy["Pieces"][indx]["y1"] + y_move
+            if in_bounds_move(copy["Pieces"][indx], new_x, new_y):
+                copy["Pieces"][indx]["x1"] += x_move
+                copy["Pieces"][indx]["x2"] += x_move
+                copy["Pieces"][indx]["y1"] += y_move
+                copy["Pieces"][indx]["y2"] += y_move
         # Rotating Mutation
-        chance_of_rotation = .1 * mutation_rate
+        chance_of_rotation = .5 * mutation_rate
         for indx in range(len(copy["Pieces"])):
             randNum = random.uniform(0.0, 1.0)
             if randNum < chance_of_rotation and in_bounds_rotate(copy["Pieces"][indx]):
-                mutated = True
-                h = copy["Piece"][indx]["height"]
-                w = copy["Piece"][indx]["width"]
-                copy["Piece"][indx]["height"] = w
-                copy["Piece"][indx]["width"] = h
+                h = copy["Pieces"][indx]["height"]
+                w = copy["Pieces"][indx]["width"]
+                copy["Pieces"][indx]["height"] = w
+                copy["Pieces"][indx]["width"] = h
+                copy["Pieces"][indx]["x2"] = copy["Pieces"][indx]["x1"] + copy["Pieces"][indx]["width"]
+                copy["Pieces"][indx]["y2"] = copy["Pieces"][indx]["y1"] + copy["Pieces"][indx]["height"]
         # If the copy was mutated add to list
-        if mutated:
-            mutated_copies.append(copy)
+        mutated_copies.append(copy)
     # Return the mutated individuals
     return mutated_copies
 
@@ -475,29 +511,40 @@ for looper in range(NUMBER_OF_GENERATIONS):
 
     # EVALUATE ALL INDIVIDUALS
     # Combine the original population with crossover and mutated individuals
-    total_population = population + offsprings + mutated_copies
+    total_population = []
+    for p in population:
+        total_population.append(p)
+    for o in offsprings:
+        total_population.append(o)
+    for m in mutated_copies:
+        total_population.append(m)
     # Calculate fitness for all individuals that havesn't been calculated
     for i in range(len(total_population)):
         if total_population[i]["Fitness1"] == None:
         	total_population[i]["Fitness1"] = fitness1(total_population[i]["Pieces"])
-        	print(total_population[i]["Fitness1"])
+        	# print(total_population[i]["Fitness1"])
         if total_population[i]["Fitness2"] == None:
             total_population[i]["Fitness2"] = fitness2(total_population[i]["Pieces"])
-        	print(total_population[i]["Fitness2"])
+            # print(total_population[i]["Fitness2"])
 
 
     # SELECT INDIVIDUALS FOR REPRODUCTION IN THE NEXT GENERATION
-    total_population = non_dominated_sort(total_population)
+    sorted_population = non_dominated_sort(total_population)
     new_population = []
-    for i in range(len(total_population)):
-        for j in range(len(total_population[i])):
-            new_population.append(total_population[i][j])
+    for i in range(len(sorted_population)):
+        for j in range(len(sorted_population[i])):
+            new_population.append(full_copy_individual(sorted_population[i][j]))
             if len(new_population) == POPULATION_SIZE:
                 break
         if len(new_population) == POPULATION_SIZE:
             break
     population = new_population
 
+    # Print highest fitness
+    print("Generation " + str(looper))
+    print("Fitness 1: " + str(population[0]["Fitness1"]))
+    print("Fitness 2: " + str(population[0]["Fitness2"]))
+    print()
 
     # Display all pieces in their new position.
     # In general, display the fittest individual of this generation.
@@ -506,20 +553,19 @@ for looper in range(NUMBER_OF_GENERATIONS):
     canvas.create_rectangle(0, 0, STOCK_WIDTH, STOCK_HEIGHT, fill='khaki')
     display_individual = population[0] # display this individual, which is a list of dictionary
     for piece_count in range(NUMBER_OF_PIECES):
-        canvas.create_rectangle(display_individual["Pieces"][piece_count].get("x1"),
-            display_individual["Pieces"][piece_count].get("y1"),
-            display_individual["Pieces"][piece_count].get("x2"),
-            display_individual["Pieces"][piece_count].get("y2"),
-            fill = display_individual["Pieces"][piece_count].get("color"),
+        canvas.create_rectangle(display_individual["Pieces"][piece_count]["x1"],
+            display_individual["Pieces"][piece_count]["y1"],
+            display_individual["Pieces"][piece_count]["x2"],
+            display_individual["Pieces"][piece_count]["y2"],
+            fill = display_individual["Pieces"][piece_count]["color"],
             outline = "black")
         #tkFont.Font(family='Helvetica',size=36, weight='bold') # http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/fonts.html
-        canvas.create_text(display_individual["Pieces"][piece_count].get("x1") + 20,
-            display_individual["Pieces"][piece_count].get("y1") + 20,
-            text=str(piece_count))
+        canvas.create_text(display_individual["Pieces"][piece_count]["x1"] + 20,
+            display_individual["Pieces"][piece_count]["y1"] + 20,
+            text=str(display_individual["Pieces"][piece_count]["id"]))
 
     canvas.update()
-    time.sleep(1) # HARDCODED TIME -- pause briefly between generations
-
+    # time.sleep(1) # HARDCODED TIME -- pause briefly between generations
 
 
 
